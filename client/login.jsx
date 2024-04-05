8c28b03b26083f79484e39efdfe9ffbb9c9ec1c3
De'Andre Perry <dmp6511@rit.edu>
2024-04-05 14:52:13 -0400

    LoginWindow built in

---
+++ client/login.jsx
@@ -0,0 +1,64 @@
+// JSX for the login page
+
+const helper = require('./helper.js');
+const React = require('react');
+const { createRoot } = require('react-dom/client');
+
+const handleLogin = (e) => {
+    e.preventDefault();
+    helper.hideError();
+
+    const username = e.target.querySelector('#user').value;
+    const pass = e.target.querySelector('#pass').value;
+
+    // make sure the fields aren't empty
+    if (!username || !pass) {
+        helper.handleError('Username or password is empty!');
+        return false;
+    };
+
+    helper.sendPost(e.target.action, { username, pass });
+    return false;
+};
+
+const handleSignup = (e) => {
+    e.preventDefault();
+    helper.hideError();
+
+    const username = e.target.querySelector('#user').value;
+    const pass = e.target.querySelector("#pass").value;
+    const pass2 = e.target.querySelector('#pass2').value;
+
+    // make sure fields aren't empty
+    if (!username || !pass || !pass2) {
+        helper.handleError("All fields are required!");
+        return false;
+    };
+
+    // make sure the passwords are the same
+    if (pass !== pass2) {
+        helper.handleError("Passwords do not match!");
+        return false;
+    }
+
+    helper.sendPost(e.target.action, { username, pass, pass2 });
+    return false;
+}
+
+const LoginWindow = (props) => {
+    return (
+        <form id='loginForm'
+            name='loginForm'
+            onSubmit={handleLogin}
+            action='/login'
+            method='POST'
+            className='mainForm'>
+
+            <label htmlFor='username'> Username: </label>
+            <input id='user' type='text' name='username' placeholder='username' />
+            <label htmlFor="pass">Password:</label>
+            <input id='pass' type="password" name='pass' placeholder='password' />
+            <input className='formSubmit' type='submit' value="Sign In" />
+        </form>
+    );
+};
